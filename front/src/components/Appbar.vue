a669e960be25f7159f416619a830cc62ed504d9b
bashpound <eunhackjang@gmail.com>
2020-09-23 18:28:33 +0900

    [Front] main page setting

--- front/src/components/Appbar.vue
+++ front/src/components/Appbar.vue
@@ -13,9 +13,11 @@
               <li><router-link to="/">Home</router-link></li>
               <li><router-link to="/gallary">갤러리</router-link></li>
               <li><router-link to="/admin">관리</router-link></li>
+              <li><router-link to="/login"> 로그인 </router-link> </li>
+              <li><router-link to="/register"> 회원가입 </router-link></li>
           </ul>
       </nav>
-      <a href="#" id="login-register-button">Login / Register</a>
+
 </div>
 </template>
 
---
+++ front/src/views/404.vue
@@ -0,0 +1,21 @@
+<template>
+  <section id="notfound">
+      <div class="flex">
+          <div class="notfound-content">
+              잘못된 요청입니다!
+          </div>
+      </div>
+  </section>
+</template>
+
+<script>
+
+export default {
+  name: '404',
+  mounted () {
+    setTimeout(() => {
+      this.$router.push('/')
+    }, 2000)
+  }
+}
+</script>
