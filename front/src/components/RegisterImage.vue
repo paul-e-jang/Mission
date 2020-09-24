2e75e287d98fb9f742745c3d139de717c0fb5d1e
bashpound <eunhackjang@gmail.com>
2020-09-25 05:35:01 +0900

    Update RegisterImage.vue

--- front/src/components/RegisterImage.vue
+++ front/src/components/RegisterImage.vue
@@ -3,20 +3,20 @@
   <div id="subject">
     <h2> 이미지 업로드 </h2>
   </div>
-  <v-row class="ma-5" v-if="imageUrl.length<1" id="grid-image">
+  <v-row class="ma-5" v-if="!images" id="grid-image">
     <v-col>
         <v-container fluid>
           <v-row>
             <v-col
-              v-for="(item, idx) in imageUrl"
-              :key="idx"
+              v-for="n in 9"
+              :key="n"
               class="d-flex child-flex"
               cols="3"
             >
               <v-card flat tile class="d-flex">
                 <v-img
-                v-if="imageUrl.length<1"
-                :src="item"
+                v-if="!images"
+                :src="imageUrl"
                 aspect-ratio="1"
                 class="grey lighten-2"
                 >
@@ -45,10 +45,10 @@
     @change="onChangeImages"
     accept="image/png, image/jpeg, image/bmp, image/jpg, image/gif"
     >
-    <v-btn rounded class="mr-3" :loading="loading" color="orange" outlined dark type="button" @click="onClickImageUpload" v-if="!imageUrl.length<1">이미지 선택
+    <v-btn rounded class="mr-3" :loading="loading" color="orange" outlined dark type="button" @click="onClickImageUpload" v-if="!images">이미지 선택
       <v-icon right dark>mdi-pencil</v-icon>
     </v-btn>
-    <v-btn rounded color="indigo" class="mr-3" outlined dark type="button" @click="onClickImageUpload" v-if="imageUrl&&!loading">다시 선택
+    <v-btn rounded color="indigo" class="mr-3" outlined dark type="button" @click="onClickImageUpload" v-if="images&&!loading">다시 선택
       <v-icon right dark>mdi-pencil</v-icon>
     </v-btn>
     <v-btn rounded color="green" :loading="loading" outlined dark type="button" @click="loader = 'loading'" v-if="imageUrl">이미지 업로드
@@ -61,7 +61,7 @@
 export default {
   data () {
     return {
-      imageUrl: [],
+      imageUrl: null,
       loading: null,
       loader: null,
       images: null
@@ -73,10 +73,7 @@ export default {
     },
     onChangeImages (e) {
       const files = e.target.files
-      this.images = files
-      files.array.forEach(element => {
-        this.imageUrl.push(URL.createObjectURL(element))
-      })
+      this.imageURL = URL.createObjectURL(files[0])
     }
   },
   watch: {
